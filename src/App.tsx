0c5c31fa0d69620dd43ca1f2f49da76ffecfb9ee
antonia <antonia@unifesspa.edu.br>
2020-09-28 18:25:37 -0300

    Api de contxt

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+
 import SignIn from './pages/SignIn';
 //import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
+import AuthContext from './context/AuthContex';
 
 const App: React.FC = () =>(
 <>
+<AuthContext.Provider value= {{name: 'Antonia' }}>
 <SignIn />
+</AuthContext.Provider>
 <GlobalStyle />
 </>
 );
