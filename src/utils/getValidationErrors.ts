ca54455bb2b4d1baf8816702de73e4a401c6fe34
antonia <antonia@unifesspa.edu.br>
2020-10-01 22:13:56 -0300

    Adiconando commit que não foram adicionado com -am até a aula estrutura de toasts

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,14 @@
+import { error } from 'console';
+import { ValidationError } from 'yup';
+
+interface Errors {
+ [key: string]: string;
+}
+export default function getValidationErros(err: ValidationError){
+   const validationErrors: Errors ={};
+
+   err.inner.forEach((error)=>{
+       validationErrors[error.path] = error.message;
+   });
+return validationErrors;
+}
